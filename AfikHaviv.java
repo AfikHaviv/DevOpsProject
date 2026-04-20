bff1fe364481a05b532d8e86c72d0fdcaf443653
Afik Haviv <afikhaviv12@gmail.com>
2026-04-20 17:31:48 +0300

    Ex3

--- AfikHaviv.java
+++ AfikHaviv.java
@@ -1,6 +1,6 @@
 public class AfikHaviv{ 
     public static void main(String[] args){
-        for (int i = 1; i <= 15; i++){
+        for (int i = 1; i <= 10; i++){
             System.out.println("My name is Afik Haviv " + i);
         }
     }
